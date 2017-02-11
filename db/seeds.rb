c19f2b781574ccff9573ccd2ebcbbc6b7a0ff1e6
张瑞兰 <zhangruilan007@gmail.com>
2017-02-11 11:18:31 +0800

    build seed page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+create_account = User.create([email: 'zhangruilan007@gmail.com', password: '123456', password_confirmation: '123456', is_admin: 'true'])
+puts "Admin account created."
+
+
+  Job.create!([title: "时尚网拍", description: "aa", wage_upper_bound: 12000, wage_lower_bound: 6000 is_hidden: "false"])
+
+
+
+  Job.create!([title: "时尚策划", description: "aa", wage_upper_bound: 13000, wage_lower_bound: 6500 is_hidden: "false"])
+
+
+
+  Job.create!([title: "形象设计", description: "aa", wage_upper_bound: 9000, wage_lower_bound: 5000 is_hidden: "false"])
+
+
+
+  Job.create!([title: "服装设计", description: "aa", wage_upper_bound: 8000, wage_lower_bound: 5000 is_hidden: "true”])
