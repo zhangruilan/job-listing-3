9f0be586b1fdf04589474570059f171b7e4ed5a7
zhangruilan007 <2535262733@qq.com>
2017-02-19 10:47:37 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-create_account = User.create([email: 'ceshi@gmail.com', password: '123456', password_confirmation: '123456', is_admin: 'true'])
-puts "Admin account created."
+cser.create(email: "ceshi@gmail.com", password: "123456", password_confirmation: "123456", is_admin: true)
 
 
-  Job.create!([title: "时尚网拍", description: "aa", wage_upper_bound: 12000, wage_lower_bound: 6000 is_hidden: "false"])
+ Job.create!(title: "时尚策划", description: "Locate/Select a vehicle best suited to the customer needs
+ Review vehicle selection, attributes, options and features
+ Conduct vehicle test drive
+ Negotiate the sale closure
+ Complete sales documentation accurately
+ Provide clear explanation of vehicle features and operation
+ Maintain a current knowledge of promotions, products, services
+ Follow up with prospective customers and return email/vocemails
+ Follow up with existing customers and generate leads", wage_lower_bound: 12000, wage_upper_bound: 15000, is_hidden: false)
 
+ Job.create!(title: "时尚网拍", description: "Locate/Select a vehicle best suited to the customer needs
+ Review vehicle selection, attributes, options and features
+ Conduct vehicle test drive
+ Negotiate the sale closure
+ Complete sales documentation accurately
+ Provide clear explanation of vehicle features and operation
+ Maintain a current knowledge of promotions, products, services
+ Follow up with prospective customers and return email/vocemails
+ Follow up with existing customers and generate leads", wage_lower_bound: 10000, wage_upper_bound: 12000, is_hidden: false)
 
+ Job.create!(title: "形象设计", description: "Locate/Select a vehicle best suited to the customer needs
+ Review vehicle selection, attributes, options and features
+ Conduct vehicle test drive
+ Negotiate the sale closure
+ Complete sales documentation accurately
+ Provide clear explanation of vehicle features and operation
+ Maintain a current knowledge of promotions, products, services
+ Follow up with prospective customers and return email/vocemails
+ Follow up with existing customers and generate leads", wage_lower_bound: 12000, wage_upper_bound: 15000, is_hidden: false)
 
-  Job.create!([title: "时尚策划", description: "aa", wage_upper_bound: 13000, wage_lower_bound: 6500 is_hidden: "false"])
-
-
-
-  Job.create!([title: "形象设计", description: "aa", wage_upper_bound: 9000, wage_lower_bound: 5000 is_hidden: "false"])
-
-
-
-  Job.create!([title: "服装设计", description: "aa", wage_upper_bound: 8000, wage_lower_bound: 5000 is_hidden: "true”])
+ Job.create!(title: "形象营销", description: "Locate/Select a vehicle best suited to the customer needs
+ Review vehicle selection, attributes, options and features
+ Conduct vehicle test drive
+ Negotiate the sale closure
+ Complete sales documentation accurately
+ Provide clear explanation of vehicle features and operation
+ Maintain a current knowledge of promotions, products, services
+ Follow up with prospective customers and return email/vocemails
+ Follow up with existing customers and generate leads", wage_lower_bound: 8000, wage_upper_bound: 10000, is_hidden: false)
